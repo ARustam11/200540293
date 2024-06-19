9e32f41c4d0365601a3b922851d83441da96d609
ARustam11 <93269378+ARustam11@users.noreply.github.com>
2024-06-19 10:51:30 -0400

    Add files via upload

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace _200540293.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _200540293.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+     
+        public string FirstName { get; set; }
+
+       
+        public string LastName { get; set; }
+
+      
+        public string EmailAddress { get; set; }
+    }
+}
+
